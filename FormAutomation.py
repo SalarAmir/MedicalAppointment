e852f42a098ca3f0c44c5fd1a7b9770c30a4e7b1
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-08-06 13:00:48 +0500

    working

--- FormAutomation.py
+++ FormAutomation.py
@@ -1,4 +1,4 @@
-from pyautogui import screenshot, locateAll, locateAllOnScreen, click, moveTo, size
+from pyautogui import screenshot, locateAll, locateAllOnScreen, click, moveTo, size, scroll, move
 import time
 from requests import post
 
@@ -79,18 +79,31 @@ class ScreenShot:
         found_texts = {}
         for text in texts_all:
             if text["description"] in texts_to_find:
-                print(tuple((vertex['x']+self.box.l, vertex['y']+self.box.t) for vertex in text["boundingPoly"]["vertices"]))
                 vertices = Box(tuple((vertex['x']+self.box.l, vertex['y']+self.box.t) for vertex in text["boundingPoly"]["vertices"]))
                 found_texts[text["description"]] = vertices
         return found_texts
-def find_all_img_in_bounds(self, image, bound_box, confidence=0.9):
-    found_coords = []
-    try:
-        ss_in_bounds = screenshot(region=(bound_box.l, bound_box.t, bound_box.w, bound_box.h))
-        found_coords =  locateAll(image, ss_in_bounds, confidence)
-        return [Box((x + bound_box.l, y + bound_box.t, w, h)) for x, y, w, h in found_coords]
-    except:
-        return []
+def find_all_img_in_bounds(image, bound_box, confidence):
+    return_l = []
+    
+    ss_in_bounds = screenshot(region=bound_box.box_tuple, imageFilename="temp.png")
+    found_coords =  list(locateAll(needleImage=image, haystackImage=ss_in_bounds, confidence=confidence))
+    # print(found_coords[0][0])
+    for i in range(len(found_coords)):
+        x, y, w, h = found_coords[i]
+        # print(x, y, w, h)
+        temp_tuple = (int(x + bound_box.l), int(y + bound_box.t), int(w), int(h))
+        # print(temp_tuple)
+        return_l.append(Box(temp_tuple))
+    return return_l
+    # return [Box((x + bound_box.l, y + bound_box.t, w, h)) for x, y, w, h in found_coords]
+    # except:
+    #     return []
+    
+# array of boxes, should be smallest x value first and smallest y value first
+def sort_boxes(boxes):
+    boxes.sort(key=lambda x: x.l)
+    boxes.sort(key=lambda x: x.t)
+    return boxes
 
 def get_annotations(ss:ScreenShot):
     with open(ss.path, 'rb') as image:
@@ -102,11 +115,6 @@ server_url = 'https://googlecloudapi.vercel.app/api/v1'
 if __name__ == "__main__":
     time.sleep(2)
     screen_size = size()
-    first_ss = ScreenShot((0, 120, screen_size[0], screen_size[1]-120-40), "first_ss.png")
-    
-    response = get_annotations(first_ss)
-    annotations = response["data"]["textAnnotations"]
-    
     form_heading_options = {
         "M1800":"02",
         "M1810":"03",
@@ -114,28 +122,53 @@ if __name__ == "__main__":
         "M1830":"05",
         "M1840":"04",
         "M1845":"00",
+        "M1850":"02",
         "M1860":"01"
     }
+    form_heading_options_index = {
+        "M1800":2,
+        "M1810":3,
+        "M1820":1,
+        "M1830":5,
+        "M1840":4,
+        "M1845":0,
+        "M1850":2,
+        "M1860":1
+        
+    }
+    remaining_forms = list(form_heading_options_index.keys())
+    while len(remaining_forms) > 0:
+        # time.sleep(2)
+        screen_ss = ScreenShot((0, 120, screen_size[0], screen_size[1]-120-40), "first_ss.png")
     
-    
-    found_headings_boxes = first_ss.find_texts(form_heading_options.keys(), annotations)
-    print(found_headings_boxes)
-    found_headings = list(found_headings_boxes.keys())
-    forms_on_screen = {}
-    for i in range(len(found_headings)-1):
-        heading_box = found_headings_boxes[found_headings[i]]
-        next_heading_box = found_headings_boxes[found_headings[i+1]]
-        form_box = Box((
-            heading_box.l,
-            heading_box.t,
-            screen_size[0] - heading_box.l,
-            next_heading_box.t - heading_box.t 
-        ))
-        forms_on_screen[found_headings[i]] = form_box
-        option_loc = first_ss.find_text_in_bounds(form_heading_options[found_headings[i]], form_box, annotations)
-        moveTo(option_loc.centre)
-        time.sleep(1)
-        # print(form_box)
-        box_ss = ScreenShot(form_box.box_tuple, f"form_{i}.png")
-    
+        response = get_annotations(screen_ss)
+        annotations = response["data"]["textAnnotations"]
+        
+        found_headings_boxes = screen_ss.find_texts(remaining_forms, annotations)
+        print(found_headings_boxes)
+        found_headings = list(found_headings_boxes.keys())
+        forms_on_screen = {}
+        for i in range(len(found_headings)-1):
+            heading_box = found_headings_boxes[found_headings[i]]
+            next_heading_box = found_headings_boxes[found_headings[i+1]]
+            
+            form_box = Box((
+                heading_box.l,
+                heading_box.t,
+                screen_size[0] - heading_box.l,
+                next_heading_box.t - heading_box.t 
+            ))
+            forms_on_screen[found_headings[i]] = form_box
+            button_locs = sort_boxes(find_all_img_in_bounds("button.png", form_box,0.9))
+            
+            moveTo(button_locs[form_heading_options_index[found_headings[i]]].centre)
+            click()
+            move(-50, 0)
+            
+            print(button_locs)
+            remaining_forms.remove(found_headings[i])
+            
+        
+        
+        scroll(-100)
     
---
+++ findScreenCoords.py
@@ -0,0 +1,2 @@
+import pyautogui as pag
+pag.screenshot(region=(360, 611, 17, 15), imageFilename="button.png")
